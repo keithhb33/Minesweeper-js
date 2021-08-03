e5589e5e062af51fcb8abd9b77f2ce0b6a761d9c
plaine1226 <61886677+plaine1226@users.noreply.github.com>
2021-08-03 15:16:27 -0400

    testing makeGrid func

---
+++ script.js
@@ -0,0 +1,16 @@
+const numMines = 2
+
+function makeGrid() {
+    var grid = new Array(4);
+    for (let i = 0; i < x.length; i++) {
+        grid[i] = new Array(4);
+    }
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            grid[i][j] = 2;
+        }
+    }
+    return grid;
+}
+
+console.log(grid);
