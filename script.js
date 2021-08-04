4b4aa8e5562f0571a4afd18ee31a676614dc6e35
keithhb33 <51885619+keithhb33@users.noreply.github.com>
2021-08-04 07:52:01 -0400

    added functions & imgs

--- script.js
+++ script.js
@@ -1,28 +1,133 @@
-var randomRow = (Math.floor(Math.random() * 4) + 1);
-var randomCol = (Math.floor(Math.random() * 4) + 1);
-const numMines = 2;
+window.onload = function() {
+
 
 function makeGrid(){
+    //Create 2d array
     var grid = new Array(4);
-    for (let i = 0; i < grid.length; i++) {
-        grid[i] = new Array(4);
+    for (let row = 0; row < 4; row++){
+        for (let column = 0; column < 4; column++){
+            grid[row] = new Array(4);
+        }
     }
+
+    //Set all squares icons to minesweeper icon png
+    for(var i=0; i<grid.length; i++){
+        for(var j=0; j<grid.length; j++){
+            grid[i][j] = document.getElementById("at-" + i.toString() + "-" + j.toString());
+            grid[i][j].src ="images/unclicked.png";
+        }
+    }
+    
+    //Click on square event
+    onSquareClick(grid);
+
+
+    //Randomize mines on board
     assignMines(grid);
+    console.log(grid);
+}
+
+
+function onSquareClick(grid){
+    var clickCounter = 0;
+        $(".container").on("click", function(event){
+
+            //Hide "click any cell to start" message
+            document.getElementById("play-message").style.opacity = "0%";
+
+            //Get clicked square element
+            var clickedSquare = event.target;
+
+            if(clickedSquare.src.includes("images/unclicked.png")){
+                clickedSquare.src = "";
+                clickCounter++;
+                console.log(clickCounter);
+            }
+            
+            //Lose game event
+            if(clickedSquare.alt == "bomb"){
+                clickedSquare.src = "images/bomb.png";
+                loseGame(grid);
+            }
+            //Win game event
+            else if(clickCounter == grid.length**2-2){
+                winGame(grid);
+            }
+        })
+}
+
+function winGame(grid){
+    //Displays bombs in grid
+    displayAllBombs(grid);
+
+    //Send win message
+    endMessage = document.getElementById("end-message");
+    endMessage.textContent = "YOU WIN!";
+}
+
+function loseGame(grid){
+    //Display all bombs in grid
+    displayAllBombs(grid);
+
+    //Send lose message
+    endMessage = document.getElementById("end-message");
+    endMessage.textContent = "YOU LOSE!";
+}
+
+
+function displayAllBombs(grid){
+    //Makes all bombs visible in grid
+    for(var i=0; i<grid.length; i++){
+        for(var j=0; j<grid.length; j++){
+            grid[i][j].src ="";
+            if(grid[i][j].alt == "bomb"){
+                grid[i][j].src = "images/bomb.png";
+            }
+        }
+    }
 }
 
 function assignMines(grid){
     //Finds random bomb location in grid
-    //Bomb values in 2D array are 1 to be accessed later
-    mine1Col = (Math.floor(Math.random() * 4) + 1);
-    mine2Col = (Math.floor(Math.random() * 4) + 1);
+    mine1Col = (Math.floor(Math.random() * 4));
+    mine2Col = (Math.floor(Math.random() * 4));
 
-    mine1Row = (Math.floor(Math.random() * 4) + 1);
-    mine2Row = (Math.floor(Math.random() * 4) + 1);
+    mine1Row = (Math.floor(Math.random() * 4));
+    mine2Row = (Math.floor(Math.random() * 4));
 
-    grid[mine1Col][mine1Row] = 1;
-    grid[mine2Col][mine2Row] = 1;
+    //Function makes sure that the two bombs can't be placed in the same spot
+    checkIfMinesSamePlace(mine1Col, mine1Row, mine2Col, mine2Row);
 
-    calculateCells(grid);
+    //Creates mines
+    grid[mine1Col][mine1Row] = document.getElementById("at-" + mine1Col.toString() + "-" + mine1Row.toString());
+    grid[mine2Col][mine2Row] = document.getElementById("at-" + mine2Col.toString() + "-" + mine2Row.toString());
+    
+    grid[mine1Col][mine1Row].alt = "bomb";
+    grid[mine2Col][mine2Row].alt = "bomb";
+
+    //Sets bombs to stay behind button images
+    grid[mine1Col][mine1Row].style.zIndex = "-1";
+    grid[mine2Col][mine2Row].style.zIndex = "-1";
+
+    grid[mine1Col][mine1Row].style.left = "50%;"
+    grid[mine2Col][mine2Row].style.top = "50px;"
+
+    console.log("Mine 1: " + grid[mine1Col][mine1Row].id);
+    console.log("Mine 2: " + grid[mine2Col][mine2Row].id);
+}
+
+function checkIfMinesSamePlace(mine1Col, mine1Row, mine2Col, mine2Row){
+    if(mine1Col == mine2Col && mine1Row == mine2Row){
+        mine1Col = (Math.floor(Math.random() * 4));
+        mine2Col = (Math.floor(Math.random() * 4));
+
+        mine1Row = (Math.floor(Math.random() * 4));
+        mine2Row = (Math.floor(Math.random() * 4));
+    }if(mine1Col == mine2Col && mine1Row == mine2Row){
+        checkIfMinesSamePlace(mine1Col, mine1Row, mine2Col, mine2Row);
+    }else{
+        return false;
+    }
 }
 
 function calculateCells(grid){
@@ -56,13 +161,9 @@ function calculateCells(grid){
             grid[r][c] = sum;
         }
     }
-checkWinLose(grid);
 }
 
-function checkWinLose(grid){
+makeGrid();
 
 }
-
-
-
 
