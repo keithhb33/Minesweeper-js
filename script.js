152cf587162692139c4c04662ec0856bcda28202
michaeleiseman <mrmike@algebrabyhand.com>
2021-08-04 21:35:27 -0400

    display proximity numbers in cells

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var grid = [];
 var bombGrid = [];
-var numBombs = 2;
+var numBombs = 0;
 var bombArray = [];
 
 function makeGrid(){
@@ -8,19 +8,20 @@ function makeGrid(){
         let rowElement = document.getElementsByClassName("row")[row];
         let rowArray = [];
         let bombRow = [];
-        for (let col = 0; col < rowElement.getElementsByClassName("col-3").length; col++) {
-            let cell = rowElement.getElementsByClassName("col-3")[col].getElementsByClassName("cell")[0];
+        for (let col = 0; col < rowElement.getElementsByClassName("col").length; col++) {
+            let cell = rowElement.getElementsByClassName("col")[col].getElementsByClassName("cell")[0];
             cell.rowIndex = row;
             cell.colIndex = col;
+            cell.addEventListener("click", userClick, false);
             rowArray.push(cell);
             bombRow.push(cell);
         }
         grid.push(rowArray);
         bombGrid.push(bombRow);
     }
+    numBombs = grid.length - 1;
 
-
-    addRemoveFlags();
+    //addRemoveFlags();
     
 
     //Randomize mines on board
@@ -40,20 +41,28 @@ function addRemoveFlags(){
 }
 
 
-$(".cell").on("click", function(){
-
+function userClick() {
+    //Remove this event listener from this cell so that nothing will happen when the user clicks on this cell
+    this.removeEventListener("click", userClick, false);
     //Hide "click any cell to start" message
     document.getElementById("play-message").style.opacity = "0%";
 
     if(bombArray.some(cell => cell === this)){
         this.getElementsByTagName("img")[0].setAttribute("src", "images/bomb.png");
+        loseGame();
     } else {
         this.getElementsByTagName("img")[0].setAttribute("src", "images/clicked.png");
+        let number = getBombProximityNumber(this);
+        if(number > 0) {
+            this.textContent = getBombProximityNumber(this).toString();
+            //this.style.setProperty("padding-top", "10px")
+        }
+        
     }
-});
+}
 
 
-function winGame(grid){
+function winGame(){
     //Displays bombs in grid
     displayAllBombs(grid);
 
@@ -62,7 +71,7 @@ function winGame(grid){
     endMessage.textContent = "YOU WIN!";
 }
 
-function loseGame(grid){
+function loseGame(){
     //Display all bombs in grid
     displayAllBombs(grid);
 
@@ -94,7 +103,8 @@ function assignMines(){
 }
 
 
-function calculateCells(grid){
+function getBombProximityNumber(cell){
+    //Mr. Mike says: This is really brilliant!!!
     let displacements = [
         [-1, -1],
         [-1, 0],
@@ -107,25 +117,22 @@ function calculateCells(grid){
     ];
     let numRows = grid.length;
     let numCols = grid[0].length;
-    for (let r = 0; r < numRows; r++) {
-        for (let c = 0; c < numCols; c++) {
-            let sum = 0;
-            for (let i = 0; i < 8; i++) //8 possible displacements 
-            {
-                let adjacentRow = r + displacements[i][0];
-                let adjacentCol = c + displacements[i][1];
-                if (!(adjacentRow < 0 || adjacentCol > numRows-1)) {
-                    if (!(adjacentCol < 0 || adjacentCol > numCols-1)) {
-                        if (grid[adjacentRow][adjacentCol] == -1) {
-                            sum++;
-                        }
-                    } 
+    let sum = 0;
+    for (let i = 0; i < 8; i++) //8 possible displacements 
+    {
+        let adjacentRowIndex = cell.rowIndex + displacements[i][0];
+        let adjacentColIndex = cell.colIndex + displacements[i][1];
+        if (!(adjacentRowIndex < 0 || adjacentRowIndex > numRows-1)) {
+            if (!(adjacentColIndex < 0 || adjacentColIndex > numCols-1)) {
+                if (bombArray.some(bomb => bomb == grid[adjacentRowIndex][adjacentColIndex])) {
+                    sum++;
                 }
-            }
-            grid[r][c] = sum;
+            } 
         }
     }
+    return sum;
 }
+
 makeGrid();
 
 
